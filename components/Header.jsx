e47edf17efc1bc2ed716380d56070394293ab103
Mark Heeling <markheelign@gmail.com>
2022-06-17 23:16:49 +0200

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="max-w-6xl px-5 py-6 mx-auto bg-white border-b-2 md:px-0">
+      <div className="flex flex-row items-center justify-between">
+        <div>
+          <a className="text-2xl font-bold">
+            Mark Heeling<span className="text-blue-700">.</span>
+          </a>
+        </div>
+
+        <div>
+          <a className="p-2 text-white bg-blue-700 rounded-sm">Contact</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col max-w-6xl mx-auto"></div>
+    </>
+  );
+}
