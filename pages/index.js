5a5e896df4c93d0cf4da689cf8e1b655975eacd9
Mark Heeling <markheelign@gmail.com>
2022-06-20 22:53:05 +0200

    Added todo list to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col max-w-6xl mx-auto">
-        <h1 className="py-10 text-4xl text-center">Hallo!</h1>
+        <h1 className="py-10 text-2xl">
+          Todo: <br />{" "}
+          <ul>
+            <li>Homepage design maken</li>
+            <li>Spotify api: Huidige liedje weergeven in footer</li>
+          </ul>
+        </h1>
       </div>
     </>
   );
