3a5643477b81a6b3139c73114cf88b2b0fa2d397
Mark Heeling <markheelign@gmail.com>
2022-06-20 23:01:47 +0200

    Test voor vercel

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="py-10 text-2xl">
           Todo: <br />{" "}
           <ul>
-            <li>Homepage design maken</li>
+            <li>Ho mepage design maken</li>
             <li>Spotify api: Huidige liedje weergeven in footer</li>
           </ul>
         </h1>
