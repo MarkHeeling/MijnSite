9ac342e7fee9a6e68ce299e8705c9c25d9d8b5c1
Mark Heeling <markheelign@gmail.com>
2022-07-05 12:07:58 +0200

    Added strapi to project

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -3,7 +3,7 @@ import Spotify from "./Spotify";
 export default function Footer() {
   return (
     <footer>
-      <div className="max-w-6xl px-5 py-6 mx-auto bg-white border-t-2 md:px-0">
+      <div className="max-w-6xl p-5 mx-auto bg-white border-t-2 md:px-0">
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row">
             <p className="text-sm">
