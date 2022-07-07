b2e38b6df9ffeb6b6f98cbca971e8ac88f275eec
Mark Heeling <markheelign@gmail.com>
2022-07-07 20:04:07 +0200

    Intro tekst

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,17 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-center max-w-6xl mx-auto">
-        <h1 className="px-5 py-10 text-2xl lg:px-0">Hallo!</h1>
+        <div className="w-2/4 py-28">
+          <h1 className="mb-10 text-2xl lg:text-5xl lg:px-0">
+            Hallo! Ik ben <br />
+            Mark Heeling
+          </h1>
+          <p>
+            Amet nisi minim dolor officia sunt excepteur ipsum. Ad quis
+            incididunt labore nulla duis ut amet magna
+          </p>
+        </div>
+        <div className="w-2/4 bg-red-100"></div>
       </div>
     </>
   );
